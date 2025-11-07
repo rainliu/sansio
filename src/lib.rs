c3398d57026e2d7dec0578fe92f57a961ee87b0f
yngrtc <y@ngr.tc>
2025-11-07 14:41:49 -0800

    add Handler

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,49 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use std::io::Result;
+use std::time::Instant;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub trait Handler {
+    /// Associated event input message type
+    type Ein: 'static;
+    /// Associated event output message type
+    type Eout: 'static;
+    /// Associated read input message type
+    type Rin: 'static;
+    /// Associated read output message type
+    type Rout: 'static;
+    /// Associated write input message type
+    type Win: 'static;
+    /// Associated write output message type for
+    type Wout: 'static;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    /// Handles Rin and returns Rout for next inbound handler handling
+    fn handle_read(&mut self, msg: Self::Rin) -> Result<()>;
+
+    /// Polls Rout from internal queue for next inbound handler handling
+    fn poll_read(&mut self) -> Option<Self::Rout>;
+
+    /// Handles Win and returns Wout for next outbound handler handling
+    fn handle_write(&mut self, msg: Self::Win) -> Result<()>;
+
+    /// Polls Wout from internal queue for next outbound handler handling
+    fn poll_write(&mut self) -> Option<Self::Wout>;
+
+    /// Handles event
+    fn handle_event(&mut self, _evt: Self::Ein) -> Result<()> {
+        Ok(())
+    }
+
+    /// Polls event
+    fn poll_event(&mut self) -> Option<Self::Eout> {
+        None
+    }
+
+    /// Handles timeout
+    fn handle_timeout(&mut self, _now: Instant) -> Result<()> {
+        Ok(())
+    }
+
+    /// Polls timeout
+    fn poll_timeout(&mut self) -> Option<Instant> {
+        None
     }
 }
